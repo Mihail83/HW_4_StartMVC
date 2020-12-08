bbb880f435ad286170c439411c78889e5b3e32fa
Mihail83 <Sklif83@tut.by>
2020-12-08 08:41:20 +0300

    Add project files.

---
+++ HW_4_StartMVC/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace HW_4_StartMVC.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
